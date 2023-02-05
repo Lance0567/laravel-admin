bee843ded747156cdadbfdd590c410116c22880f
Lance Madel. Esurena <lancemadel.esurena@ust-legazpi.edu.ph>
2023-02-05 18:26:44 +0800

    Added a logout function

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -665,6 +665,17 @@
               <p>Informational</p>
             </a>
           </li>
+          <li class= "nav-item">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+             {{ __('Logout') }}
+         </a>
+
+         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+             @csrf
+         </form>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
