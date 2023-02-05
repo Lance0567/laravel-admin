421ffac34730b5019db22a6754906c81b9bec04c
Lance Madel. Esurena <lancemadel.esurena@ust-legazpi.edu.ph>
2023-02-05 20:01:23 +0800

    Removed dropdown menu

--- resources/views/backend/layouts/dashboard.blade.php
+++ resources/views/backend/layouts/dashboard.blade.php
@@ -101,13 +101,7 @@
                     <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
                       <i class="fas fa-wrench"></i>
                     </button>
-                    <div class="dropdown-menu dropdown-menu-right" role="menu">
-                      <a href="#" class="dropdown-item">Action</a>
-                      <a href="#" class="dropdown-item">Another action</a>
-                      <a href="#" class="dropdown-item">Something else here</a>
-                      <a class="dropdown-divider"></a>
-                      <a href="#" class="dropdown-item">Separated link</a>
-                    </div>
+                    
                   </div>
                   <button type="button" class="btn btn-tool" data-card-widget="remove">
                     <i class="fas fa-times"></i>
@@ -119,7 +113,6 @@
                 <div class="row">
                   <div class="col-md-8">
                     
-              </div>
               <!-- /.card-footer -->
             </div>
             <!-- /.card -->
