4ecb325b61817f6e257fe6dd72357bc04d2a902c
Lance Madel. Esurena <lancemadel.esurena@ust-legazpi.edu.ph>
2023-02-05 20:32:57 +0800

    Fix in dashboard

--- resources/views/backend/layouts/dashboard.blade.php
+++ resources/views/backend/layouts/dashboard.blade.php
@@ -112,7 +112,8 @@
               <div class="card-body">
                 <div class="row">
                   <div class="col-md-8">
-                    
+
+                </div>
               <!-- /.card-footer -->
             </div>
             <!-- /.card -->
