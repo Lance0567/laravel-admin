bf0735fa028acc9200c6dbab8752277ef62446c8
Lance Madel. Esurena <lancemadel.esurena@ust-legazpi.edu.ph>
2023-02-07 20:56:10 +0800

    Updating data tables codes

--- resources/views/backend/datatables.blade.php
+++ resources/views/backend/datatables.blade.php
@@ -1,1 +1,25 @@
-this is data table.
+@extends('backend.layouts.app')
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Info boxes -->
+        <div class="row">
+          
+        this is data tables file
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+        
+      </div><!--/. container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  @endsection
